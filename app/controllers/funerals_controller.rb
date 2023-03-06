f661fec0b0b81227dab47c24937655b9acc2a423
canozben <canozben@windowslive.com>
2023-03-06 17:40:03 +0100

    models created

--- app/controllers/funerals_controller.rb
+++ app/controllers/funerals_controller.rb
@@ -1,2 +1,11 @@
 class FuneralsController < ApplicationController
+  def show
+    @funeral = Funeral.find(params[:id])
+  end
+
+  private
+
+  def funeral_params
+    params.require(:funeral).permit(:user_id)
+  end
 end
