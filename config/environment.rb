27dd701f63fb865c28853679bda875a9838ec2b9
Malalaya <sia.brandl@gmail.com>
2023-03-09 15:40:29 +0100

    comment out action-mailer

--- config/environment.rb
+++ config/environment.rb
@@ -4,14 +4,13 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['WINDOWSLIVE_USERNAME'],
-  :password => ENV['WINDOWSLIVE_PASSWORD'],
-  :domain => 'windowslive.com',
-  :address => 'smtp.windowslive.com',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+# ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.perform_deliveries = true
+# ActionMailer::Base.smtp_settings = {
+  # :user_name => ENV['WINDOWSLIVE_USERNAME'],
+  # :password => ENV['WINDOWSLIVE_PASSWORD'],
+  # :domain => 'windowslive.com',
+  # :address => 'smtp.windowslive.com',
+  # :port => 587,
+  # :authentication => :plain,
+  # :enable_starttls_auto => true }
