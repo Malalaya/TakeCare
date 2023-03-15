123a7a89e3c18b83780570693575d88591562ca8
Malalaya <sia.brandl@gmail.com>
2023-03-15 12:46:26 +0100

    ba

--- db/schema.rb
+++ db/schema.rb
@@ -129,7 +129,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_104325) do
     t.date "birthday"
     t.text "description"
     t.string "background_picture"
-    t.jsonb "notification_settings"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
